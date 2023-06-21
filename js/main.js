5436cf4fb7699cc374ddf47130399bea3af0b22a
Krytobok <102590165+Krytobok@users.noreply.github.com>
2023-06-21 09:38:46 +0300

    main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var myImage = document.querySelector('img');
 var images = ['img/Form111.png', 'img/Form112.png', 'img/form3.png', 'img/Form114.png'];
 var currentIndex = 0;
-
+ 
 myImage.onclick = function() {
   currentIndex = (currentIndex + 1) % images.length;
   myImage.setAttribute('src', images[currentIndex]);
